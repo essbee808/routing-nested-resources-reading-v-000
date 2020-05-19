994859949a9fc66e5e862b06ab6d4d6f176f8f8c
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-19 18:10:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   #   #nested resource for posts
   #   resources :posts, only: [:show, :index]
   # end
-  # 
+  #
   # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'

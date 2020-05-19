e2a7110fc697becee66afa14ba4a5d92e0e49564
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-19 18:02:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,6 @@ Rails.application.routes.draw do
     #nested resource for posts
     resources :posts, only: [:show, :index]
   end
-  get 'authors/:id/posts', to: 'authors#posts_index'
-
-  get 'authors/:id/posts/post_id', to: 'authors#post'
-
-  resources :authors, only: [:show]
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 

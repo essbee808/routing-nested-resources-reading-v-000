fdce89815895af948a301e1a12f9f9e387b127ae
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-19 17:43:20 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,7 @@ class AuthorsController < ApplicationController
     #notice that because ids are unique by table we can go directly to
     # Post.find - no need for @author.posts.find...
     @post = Post.find(params[:post_id])
+    render template: 'posts/show'
   end
 
   def show

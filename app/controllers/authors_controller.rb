1896dad88fb0d7a38e51da0da0046d4b959d8a7a
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-19 17:41:50 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,9 @@
 class AuthorsController < ApplicationController
 
   def posts_index
+    @author = Author.find(params[:id])
+    @posts = @author.posts
+    render template: 'posts/index'
   end
 
   def show

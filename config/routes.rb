560faffad33522b704a77f18140fd3d2db999975
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-19 18:10:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
     resources :posts, only: [:show, :index]
   end
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
+  # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  #
   # root 'posts#index'
 
 end
